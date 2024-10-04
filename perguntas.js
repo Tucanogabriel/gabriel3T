f4a1213976e3965ad09f0425bd343f824a073e74
ticas <133021668+Tucanogabriel@users.noreply.github.com>
2024-10-04 10:32:31 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,63 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    '1-Medicina',
+    ' Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros. São retirados 450 mililitros'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    '2-Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    '3-Historia',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    '4-Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
+criaCartao(
+    '5-Literatura',
+    'Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Dom Quixote'
+)
+criaCartao(
+    '6-Matematica',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
+criaCartao(
+    '7-Quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    ' 118'
+)
+criaCartao(
+    '8-Geografia',
+    ' Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    'Japão e Serra Leoa'
+)
+criaCartao(
+    '9-Ingles',
+    'O que a palavra legend significa em português?',
+    'Lenda'
+)
+criaCartao(
+    '10-Educação fisica',
+    ' Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+    '7'
+)
+criaCartao(
+    '11-Artes',
+    'Quem pintou "Guernica"?',
+    'Pablo Picasso'
+)
+criaCartao(
+    '12-Literatura',
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da Neblina'
 )
